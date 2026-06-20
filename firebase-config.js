53b2dfdc92ff986f702fd014dfbcdf2e2cb11fab
gormprojects <jerome.b.brunet@gmail.com>
2026-06-20 22:54:01 +0200

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyA-Qj8Gs1q1YQyCl4_fsXr1B6AiSENgBxQ",
-  authDomain: "coprovigie.firebaseapp.com",
-  projectId: "coprovigie",
-  storageBucket: "coprovigie.firebasestorage.app",
-  messagingSenderId: "554531936535",
-  appId: "1:554531936535:web:d7daab50ae5a6ac05b3f52",
-};
